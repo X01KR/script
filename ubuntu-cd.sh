a168e90ce0a6fc2ee75683ee7a16b32c30c585aa
X01 <x01@x01.kr>
2025-08-02 23:28:55 +0900

    ubuntu-cd.sh 업데이트

--- ubuntu-cd.sh
+++ ubuntu-cd.sh
@@ -32,3 +32,31 @@ mkdir -p /mirror/ubuntu-releases
 
 ACTUAL_USER=${SUDO_USER:-$USER}
 chown $ACTUAL_USER:$ACTUAL_USER /mirror/ubuntu-releases
+
+fatal() {
+  echo "$1"
+  exit 1
+}
+
+warn() {
+  echo "$1"
+}
+
+# Find a source mirror near you which supports rsync on
+# https://launchpad.net/ubuntu/+cdmirrors
+# rsync://<iso-country-code>.rsync.releases.ubuntu.com/releases should always work
+RSYNCSOURCE=rsync://ftp.kaist.ac.kr/ubuntu-cd
+
+# Define where you want the mirror-data to be on your mirror
+BASEDIR=/mirror/ubuntu-releases
+
+if [ ! -d ${BASEDIR} ]; then
+  warn "${BASEDIR} does not exist yet, trying to create it..."
+  mkdir -p ${BASEDIR} || fatal "Creation of ${BASEDIR} failed."
+fi
+
+rsync --verbose --recursive --times --links --safe-links --hard-links \
+  --stats --delete-after \
+  ${RSYNCSOURCE} ${BASEDIR} || fatal "Failed to rsync from ${RSYNCSOURCE}."
+
+date -u > ${BASEDIR}/.trace/$(hostname -f)
