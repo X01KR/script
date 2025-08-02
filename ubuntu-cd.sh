0ed2959a7ef26f116d9761e5d410e2d9fcc09d36
X01 <x01@x01.kr>
2025-08-02 20:48:36 +0900

    ubuntu-cd.sh 만들기

---
+++ ubuntu-cd.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+apt install rsync nginx -y
+
+rm /etc/nginx/sites-available/default
+rm /etc/nginx/sites-enabled/default
+
+cd /etc/nginx/sites-enabled/
+
+wget https://raw.githubusercontent.com/x01kr/script/main/nginx-ubuntu-cd.conf
+
+nginx -s reload
+
+mkdir -p /mirror/ubuntu-releases
+
+chown $USER:$USER /mirror/ubuntu-releases
+
+rsync -av --delete --progress rsync://ftp.kaist.ac.kr/ubuntu-cd/ /mirror/ubuntu-releases/
