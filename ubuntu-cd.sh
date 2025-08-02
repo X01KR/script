b1e0c97c32fc41650cc017a2fff3ed1b92a72bf3
X01 <x01@x01.kr>
2025-08-02 20:58:45 +0900

    ubuntu-cd.sh 업데이트

--- ubuntu-cd.sh
+++ ubuntu-cd.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
 apt update
+
+echo apt updated
+
 apt install rsync nginx -y
+
+echo installed rsync, nginx
 
 rm /etc/nginx/sites-available/default
 rm /etc/nginx/sites-enabled/default
 
+echo removed default nginx config
+
 cd /etc/nginx/sites-enabled/
 
-wget https://raw.githubusercontent.com/x01kr/script/main/nginx-ubuntu-cd.conf
+wget https://raw.githubusercontent.com/x01kr/script/main/nginx-ubuntu-cd.conf >> /dev/null
+
+echo downloaded nginx config for mirror
 
 nginx -s reload
 
+echo success to reload nginx
+
 mkdir -p /mirror/ubuntu-releases
+
+echo made /mirror/ubuntu-releases
 
 chown $USER:$USER /mirror/ubuntu-releases
 
