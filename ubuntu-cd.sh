f2cf17a4ad5506b58933a6a5ec04bddc79aff47c
X01 <x01@x01.kr>
2025-08-03 09:31:43 +0900

    ubuntu-cd.sh 업데이트

--- ubuntu-cd.sh
+++ ubuntu-cd.sh
@@ -33,8 +33,17 @@ mkdir -p /mirror/ubuntu-releases
 ACTUAL_USER=${SUDO_USER:-$USER}
 chown $ACTUAL_USER:$ACTUAL_USER /mirror/ubuntu-releases
 
+mkdir -p /data/mirror/script
+
+cd /data/mirror/script
+
 wget -O ubuntu-cd-sync.sh https://raw.githubusercontent.com/x01kr/script/main/ubuntu-cd-sync.sh
 
 chmod +x ubuntu-cd-sync.sh
 
+CRON_JOB='0 4,10,16,22 * * * /data/mirror/script/ubuntu-cd-sync.sh >> /data/mirror/ubuntu-releases-sync.log'
+
+(crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
+
 ./ubuntu-cd-sync.sh
+
