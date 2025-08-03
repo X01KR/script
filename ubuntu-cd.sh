30c10423e3a22a98a481e24568ab49956b984d36
X01 <x01@x01.kr>
2025-08-03 09:44:12 +0900

    ubuntu-cd.sh 업데이트

--- ubuntu-cd.sh
+++ ubuntu-cd.sh
@@ -9,8 +9,8 @@ fi
 echo “Updating packages...”
 apt update
 
-echo “Installing rsync, nginx...”
-apt install rsync nginx -y
+echo “Installing wget, rsync, nginx...”
+apt install rsync nginx wget -y
 
 echo “Removing default nginx config...”
 rm -f /etc/nginx/sites-available/default
@@ -29,21 +29,25 @@ systemctl reload nginx
 echo “Create mirror directory...”
 mkdir -p /mirror/ubuntu-releases
 
-
+echo "Changing Permission..."
 ACTUAL_USER=${SUDO_USER:-$USER}
 chown $ACTUAL_USER:$ACTUAL_USER /mirror/ubuntu-releases
 
+echo "Create Script directory..."
 mkdir -p /data/mirror/script
-
 cd /data/mirror/script
 
+echo "Downloading syncing script..."
 wget -O ubuntu-cd-sync.sh https://raw.githubusercontent.com/x01kr/script/main/ubuntu-cd-sync.sh
 
+echo "Changing Permission for syncing script..."
 chmod +x ubuntu-cd-sync.sh
 
+echo "Add script to Cron"
 CRON_JOB='0 4,10,16,22 * * * /data/mirror/script/ubuntu-cd-sync.sh >> /data/mirror/ubuntu-releases-sync.log'
 
 (crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
 
+echo "Syncing..."
 ./ubuntu-cd-sync.sh
 
